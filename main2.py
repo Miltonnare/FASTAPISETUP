5a5b7a4470e87c1b1da274b9b518a4319d680e94
Miltonnare <miltonnareblessed@gmail.com>
2026-01-28 20:21:35 +0300

    Everything fast

---
+++ main2.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel  
+
+app=FastAPI
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price:float
+    quantity:int=1
+    tax: Optional[float] = 0.0
+    
+@app.post("/items/" )
+async def create_item(item: Item):
+   subtotal=item.price * item.quantity
+   total_tax=item.tax*item.quantity
+   total_price= subtotal + total_tax
+   
+   return{
+       "name":item.name,
+       "quantity":item.quantity,
+       "unit Price":item.price,
+       "tax per item":item.tax,
+       "subtotal":subtotal,
+       "total tax":total_tax,
+       "total price":total_price
+   }
